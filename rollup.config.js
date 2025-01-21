68d652833ac767396d3c24ac7128b13ab5f4c775
Alvarooo7 <alvarofranco2020@gmail.com>
2025-01-20 20:31:59 -0500

    chore: Modify dependencies

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,8 @@
 import typescript from 'rollup-plugin-typescript2';
 import resolve from '@rollup/plugin-node-resolve';
 import multiEntry from '@rollup/plugin-multi-entry';
+import commonjs from '@rollup/plugin-commonjs';
+import json from '@rollup/plugin-json';
 
 export default {
   input: ['src/app.ts', 'src/flows/**/*.ts'], // Incluye los flows
@@ -9,6 +11,8 @@ export default {
     format: 'esm',
   },
   plugins: [
+    json(), // Agrega el plugin JSON aquí
+    commonjs(),
     resolve(),
     typescript(),
     multiEntry(), // Permite múltiples entradas
